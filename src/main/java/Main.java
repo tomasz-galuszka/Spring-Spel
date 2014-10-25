ee32ffd2f55d6787f47b3420e1879f1112b4d966
tomasz <tomgal144@gmail.com>
2014-10-25 13:35:37 +0200

    spel-part3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spel.Part1;
 import spel.Part2;
+import spel.beans.SimpleCup;
 
 /**
  * Created by tomasz on 25.10.14.
@@ -9,5 +13,13 @@ public class Main {
     public static void main(String[] args) {
         new Part1().run();
         new Part2().run();
+
+        // Part 3 spel in xml context
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] {"context.xml"});
+        SimpleCup whiteCup = (SimpleCup) ctx.getBean("whiteCup");
+        System.out.println(whiteCup);
+
+        ((AbstractApplicationContext)ctx).registerShutdownHook();
+
     }
 }
--- src/main/java/spel/beans/SimpleCup.java
+++ src/main/java/spel/beans/SimpleCup.java
@@ -6,12 +6,34 @@ package spel.beans;
 public class SimpleCup {
 
     private String color;
+    private int capacity;
 
     public SimpleCup(String color) {
         this.color = color;
     }
 
+    public SimpleCup(String color, int capacity) {
+        this.color = color;
+        this.capacity = capacity;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleCup{" +
+                "color='" + color + '\'' +
+                ", capacity=" + capacity +
+                '}';
     }
 }
